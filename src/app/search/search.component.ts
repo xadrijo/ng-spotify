4bb375a07cf60bd622b8bdfc6c48a034a3079f31
Jorge Jimenez <george@shocklogic.com>
2017-03-12 11:57:06 -0400

    Added search API request

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SpotifyService } from '../spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  searchStr: string;
 
-  constructor() { }
+  constructor(private _spotifyService:SpotifyService) {
 
-  ngOnInit() {
+  }
+
+  searchMusic(){
+    this._spotifyService.searchMusic(this.searchStr).subscribe(res => {
+      console.log(res);
+    });
   }
 
 }
